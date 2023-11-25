30f30078ee812c70aadc0270e52cc316893a68f6
Vaishali Vyas <sayvilahsiav@outlook.com>
2023-11-25 18:02:39 +0530

    Implemented invertImage method

--- BatchImageInverter.java
+++ BatchImageInverter.java
@@ -1,4 +1,4 @@
-
+import edu.duke.*;
 /**
  * Write a description of BatchImageInverter here.
  * 
@@ -6,5 +6,27 @@
  * @version 2023, November 25th
  */
 public class BatchImageInverter {
+    
+    public ImageResource invertImage(ImageResource inImage) {
+        ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
+        for (Pixel pixel : outImage.pixels()) {
+            Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
+            pixel.setRed(255 - inPixel.getRed());
+            pixel.setGreen(255 - inPixel.getGreen());
+            pixel.setBlue(255 - inPixel.getBlue());
+        }
+        return outImage;
+    }
+    
+    public void testInvertImage() {
+        ImageResource ir = new ImageResource();
+        ImageResource inverted = invertImage(ir);
+        inverted.draw();
+    }
+    
+    public static void main(String[] args) {
+        BatchImageInverter bii = new BatchImageInverter();
+        bii.testInvertImage();
+    }
     
 }
